bfb25a8ce70ec67ef6f3fa9d462123f2092cd4e2
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-07-10 21:14:02 -0700

    Reset increment to 1

---
+++ prisma/migrations/20240711041238_reset_increment/migration.sql
@@ -0,0 +1,3 @@
+-- This is an empty migration.
+
+ALTER SEQUENCE "User_id_seq" RESTART WITH 1;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
     }
 
     main();
-  }, [router]);
+  }, []);
 
   return (
     <div
