34809b526c210d8480f6ca39e12d98c650c2abec
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-07-10 23:15:47 -0700

    Unexpose client secret to client

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: NextRequest, response: Response) {
   const resp = await axios
     .post(`https://quavergame.com/oauth2/token`, {
       client_id: process.env.NEXT_PUBLIC_QUAVER_CLIENT_ID,
-      client_secret: process.env.NEXT_PUBLIC_QUAVER_CLIENT_SECRET,
+      client_secret: process.env.QUAVER_CLIENT_SECRET,
       grant_type: "client_credentials",
       code,
     })
@@ -29,7 +29,7 @@ export async function POST(request: NextRequest, response: Response) {
       { code: access_token },
       {
         headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_QUAVER_CLIENT_SECRET}`,
+          Authorization: `Bearer ${process.env.QUAVER_CLIENT_SECRET}`,
         },
       }
     )
--- src/app/api/user-token/route.ts
+++ src/app/api/user-token/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest } from 'next/server';
+import { NextRequest } from "next/server";
 
-import axios from 'axios';
+import axios from "axios";
 
 export async function POST(request: NextRequest, response: Response) {
   const body = await request.json();
@@ -10,29 +10,29 @@ export async function POST(request: NextRequest, response: Response) {
   const resp = await axios
     .post(`https://quavergame.com/oauth2/token`, {
       client_id: process.env.NEXT_PUBLIC_QUAVER_CLIENT_ID,
-      client_secret: process.env.NEXT_PUBLIC_QUAVER_CLIENT_SECRET,
-      grant_type: 'client_credentials',
+      client_secret: process.env.QUAVER_CLIENT_SECRET,
+      grant_type: "client_credentials",
       code,
     })
-    .catch((e) => console.log('Error 1'));
+    .catch((e) => console.log("Error 1"));
 
   const access_token = resp?.data.access_token;
 
   if (!access_token) {
-    return Response.json({ status: 400, msg: 'Access code not found.' });
+    return Response.json({ status: 400, msg: "Access code not found." });
   }
   const resp2 = await axios
     .post(
-      'https://quavergame.com/oauth2/me',
+      "https://quavergame.com/oauth2/me",
       { code: access_token },
       {
         headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_QUAVER_CLIENT_SECRET}`,
+          Authorization: `Bearer ${process.env.QUAVER_CLIENT_SECRET}`,
         },
       }
     )
     .then((resp) => resp)
-    .catch((e) => console.log('Error 2'));
+    .catch((e) => console.log("Error 2"));
 
   return Response.json({ status: 200, data: resp2?.data });
 }
