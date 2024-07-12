2a67df438e287a83d177b087407a451367cb68f6
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-07-12 10:17:50 -0700

    Move to baseURL env

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,64 +6,64 @@ import { useEffect, useState } from "react";
 import User from "@/interfaces/user";
 
 export default function LoginPage() {
-  const [loadingText, setText] = useState<string>("Logging you in...");
-  const params = useSearchParams();
-  const router = useRouter();
+    const [loadingText, setText] = useState<string>("Logging you in...");
+    const params = useSearchParams();
+    const router = useRouter();
 
-  useEffect(() => {
-    const code = params.get("code");
+    useEffect(() => {
+        const code = params.get("code");
 
-    async function main() {
-      const resp = await fetch("/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ code: code }),
-      }).then((resp) => resp.json());
+        async function main() {
+            const resp = await fetch("/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ code: code }),
+            }).then((resp) => resp.json());
 
-      if (resp.status !== 200) {
-        setText(resp.message);
-        router.push("/");
-        router.refresh();
-        return;
-      }
+            if (resp.status !== 200) {
+                setText(resp.message);
+                router.push("/");
+                router.refresh();
+                return;
+            }
 
-      setText("Done!");
-      console.log(resp);
-      setLocalStorage(resp.user);
-      router.push("/");
-      router.refresh();
-    }
+            setText("Done!");
+            console.log(resp);
+            setLocalStorage(resp.user);
+            router.push("/");
+            router.refresh();
+        }
 
-    main();
-  }, []);
+        main();
+    }, []);
 
-  return (
-    <div
-      style={{
-        position: "fixed",
-        width: "100vw",
-        height: "100vh",
-        top: 0,
-        left: 0,
-        color: "white",
-        fontSize: "5rem",
-        zIndex: "69",
-        display: "grid",
-        placeItems: "center",
-        textShadow: "2px 2px 5px black",
-      }}
-    >
-      {loadingText}
-    </div>
-  );
+    return (
+        <div
+            style={{
+                position: "fixed",
+                width: "100vw",
+                height: "100vh",
+                top: 0,
+                left: 0,
+                color: "white",
+                fontSize: "5rem",
+                zIndex: "69",
+                display: "grid",
+                placeItems: "center",
+                textShadow: "2px 2px 5px black",
+            }}
+        >
+            {loadingText}
+        </div>
+    );
 }
 
 function setLocalStorage(user: User) {
-  localStorage.setItem("id", user.id.toString());
-  localStorage.setItem("username", user.username);
-  localStorage.setItem("quaver_id", user.quaver_id.toString());
-  localStorage.setItem("avatar", user.avatar);
-  localStorage.setItem("hash", user.hash);
+    localStorage.setItem("id", user.id.toString());
+    localStorage.setItem("username", user.username);
+    localStorage.setItem("quaver_id", user.quaver_id.toString());
+    localStorage.setItem("avatar", user.avatar);
+    localStorage.setItem("hash", user.hash);
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,19 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-
     // const url = req.nextUrl
     // const { pathname } = url
-
     // if (pathname.startsWith(`/api/`)) {
     //     console.log(req.headers.get("referer"))
-    //     if (!req.headers.get("referer")?.includes(process.env.NEXT_PUBLIC_REDIRECT_URI?.replace("/login", "") as string)) {
+    //     if (!req.headers.get("referer")?.includes(process.env.NEXT_PUBLIC_BASE_URL as string)) {
     //     return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     //     }
     //   }
-
     //  return NextResponse.next()
-
 }
 
 export const config = {
-  matcher: ['/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)'],
-}
+    matcher: ["/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)"],
+};
