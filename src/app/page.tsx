951e5120ef9889b1ff17dd9b4a1d235726528cd4
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-07-13 12:35:45 -0700

    Test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-'use client';
+"use client";
 
-import { Title } from '@/components/Typography/typography';
-import '../styles/global.scss';
-
+import { Title } from "@/components/Typography/typography";
+import "../styles/global.scss";
 
 export default function HomePage() {
- 
-  return (
-    <>
-      <main>
-        <Title>don&apos;t know what to put here</Title>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main>
+                <Title>zeph will design this later LOL</Title>
+            </main>
+        </>
+    );
 }
