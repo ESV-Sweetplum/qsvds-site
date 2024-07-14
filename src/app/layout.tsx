5dc814d0cdccf467266cdde27bd6e8ce762bacd5
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-07-14 16:42:34 -0700

    Import radix and Inter, redesign home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import { Inter } from "next/font/google";
+import "@radix-ui/themes/styles.css";
 
 import NavBar from "@/components/NavBar";
 import { CookiesProvider } from "next-client-cookies/server";
 import { Analytics } from "@vercel/analytics/react";
+import { Theme } from "@radix-ui/themes";
 
 export const metadata = {
     title: "QuaverSV",
     description: "I have literally no idea what I'm doing, send help.",
 };
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export default function RootLayout({
     children,
@@ -17,13 +19,22 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
+        <html lang="en" className={inter.variable}>
             <body>
-                <CookiesProvider>
-                    <NavBar />
-                    {children}
-                    <Analytics />
-                </CookiesProvider>
+                <Theme
+                    accentColor="violet"
+                    grayColor="mauve"
+                    panelBackground="translucent"
+                    scaling="100%"
+                    radius="full"
+                    appearance="dark"
+                >
+                    <CookiesProvider>
+                        <NavBar />
+                        {children}
+                        <Analytics />
+                    </CookiesProvider>
+                </Theme>
             </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
-
-import { Title } from "@/components/Typography/typography";
+import Image from "next/image";
+import Logo from "../../public/logo.svg";
+import styles from "./index.module.scss";
+import { Button, Text } from "@radix-ui/themes";
 import "../styles/global.scss";
 import { useEffect, useState } from "react";
 import { useMouse, useWindowSize } from "@uidotdev/usehooks";
+import { DoubleArrowRightIcon } from "@radix-ui/react-icons";
 
 export default function HomePage() {
     const [state, _] = useMouse();
@@ -18,8 +21,22 @@ export default function HomePage() {
 
     return (
         <>
+            <div className={styles.vignette} />
+            <div className={styles.background} />
             <main>
-                <Title>zeph will design this later LOL</Title>
+                <section className={styles.lander}>
+                    <Text style={{ fontSize: "9rem" }} weight="light">
+                        QS
+                        <Image src={Logo} alt="bruh" width={108} height={108} />
+                        DS
+                    </Text>
+                    <Button className={styles.landerButton} radius="large">
+                        <Text size="4" color="iris">
+                            Progression Guide
+                        </Text>
+                        <DoubleArrowRightIcon />
+                    </Button>
+                </section>
             </main>
         </>
     );
