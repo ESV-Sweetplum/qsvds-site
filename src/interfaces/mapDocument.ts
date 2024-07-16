53734091d594b80c23ab2601c7b4ca3669cd623e
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2024-07-16 14:52:03 -0700

    Fix map document interface

--- src/interfaces/mapDocument.ts
+++ src/interfaces/mapDocument.ts
@@ -2,8 +2,8 @@ import { Category } from "./category";
 import Map from "./map";
 
 export default interface MapDocument {
-    id: number;
-    map: Map;
+    map_id: number;
+    mapQua: Map;
     submittedBy_id: number;
     quaver_id: number;
     totalRating: number;
