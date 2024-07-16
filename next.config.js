bb75420d3024bc2512025ecab0f247f1f5c330fc
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2024-07-16 15:30:20 -0700

    CSP Header test with next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
 const headers = async () => [
     {
         // matching all API routes
@@ -41,5 +54,18 @@ module.exports = {
     reactStrictMode: false,
     compiler: {
         removeConsole: process.env.NODE_ENV === "production",
-    }
+    },
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: cspHeader.replace(/\n/g, ''),
+              },
+            ],
+          },
+        ]
+      },
 };
