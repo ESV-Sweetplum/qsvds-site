08a2a55226e46caec43fc2f254d7b154b3f0f1cd
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-07-16 21:14:13 -0700

    Move rating API to upsert with composite query

--- src/app/api/rating/route.ts
+++ src/app/api/rating/route.ts
@@ -11,52 +11,43 @@ export async function POST(request: NextRequest) {
 
     const rating = _.clamp(parseInt(body.rating), 0, 60);
 
-    const userExists = !!prisma.user.findFirst({
+    const existingUser = !!prisma.user.findFirst({
         where: { user_id: parseInt(body.user_id) },
     });
 
-    const mapExists = await prisma.map.findFirst({
+    const existingMap = await prisma.map.findFirst({
         where: { quaver_id: parseInt(body.map_id) },
     });
 
-    if (!userExists)
+    if (!existingUser)
         return Response.json({ status: 404, message: "User does not exist" });
 
-    if (!mapExists)
+    if (!existingMap)
         return Response.json({ status: 404, message: "Map does not exist" });
 
-    if (mapExists.baseline)
+    if (existingMap.baseline)
         return Response.json({
             status: 401,
             message: "You cannot edit a baseline map.",
         });
 
-    const existingRating = await prisma.rating.findFirst({
+    await prisma.rating.upsert({
         where: {
+            user_id_map_id: {
+                user_id: parseInt(body.user_id) ?? 0,
+                map_id: existingMap.map_id ?? 0,
+            },
+        },
+        update: {
+            rating: rating,
+        },
+        create: {
             user_id: parseInt(body.user_id),
-            map_id: mapExists.map_id,
+            map_id: existingMap.map_id,
+            quality: body.quality ?? "Decent",
+            rating: rating,
         },
     });
-
-    if (existingRating) {
-        await prisma.rating.update({
-            where: {
-                rating_id: existingRating.rating_id,
-            },
-            data: {
-                rating: rating,
-            },
-        });
-    } else {
-        await prisma.rating.create({
-            data: {
-                user_id: parseInt(body.user_id),
-                map_id: mapExists.map_id,
-                quality: body.quality ?? "Decent",
-                rating: rating,
-            },
-        });
-    }
 
     const aggregateRating = await prisma.rating
         .aggregate({
@@ -64,14 +55,14 @@ export async function POST(request: NextRequest) {
                 rating: true,
             },
             where: {
-                map_id: mapExists.map_id,
+                map_id: existingMap.map_id,
             },
         })
         .then(d => d._avg.rating);
 
     const updateMap = await prisma.map.update({
         where: {
-            map_id: mapExists.map_id,
+            map_id: existingMap.map_id,
         },
         data: {
             totalRating: aggregateRating ?? 0,
@@ -80,7 +71,7 @@ export async function POST(request: NextRequest) {
 
     const newRatings = await prisma.rating.findMany({
         where: {
-            map_id: mapExists.map_id,
+            map_id: existingMap.map_id,
         },
     });
 
