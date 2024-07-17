9feb99c426535046a42203e65c8af132f1e8a9df
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2024-07-17 13:17:12 -0700

    Fix xp computation algorithm

--- src/app/api/compute-xp/route.ts
+++ src/app/api/compute-xp/route.ts
@@ -22,11 +22,13 @@ export async function POST(request: NextRequest) {
       xp
     }
   })
+
+  return Response.json({status: 200, xp})
 }
 
 function xpFormula(mapRating: number, category: Category, accuracy: number, rate: number) {
-  const thresholds = [70, 80,  85,  90,  93,   95,   97,  98,   99]
-  const bias =       [0.1, 0.5, 0.6, 0.7, 0.77, 0.82, 0.9, 0.95, 1]
+  const thresholds = [0, 70, 80,  85,  90,  93,   95,   97,  98,   99, 100]
+  const bias =       [0, 0.1, 0.5, 0.6, 0.7, 0.77, 0.82, 0.9, 0.95, 1, 1]
   
   let bottom = 0
   
