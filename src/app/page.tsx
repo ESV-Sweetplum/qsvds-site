77a13833385fc982f80584c40a9e184f17a497a9
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-07-16 20:01:28 -0700

    Revert "Attempt mask canvasless"
    This reverts commit b0a92184a9c4ca3e88f015590d291e7a9490bd4a.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import TwirlyThing from "../../public/twirlything.svg";
 import Logo from "../../public/logo.svg";
 import Calligraphy from "../../public/calligraphy.svg";
 import styles from "./index.module.scss";
@@ -25,19 +24,102 @@ export default function HomePage() {
     const [fade, setFade] = useState<boolean>(false);
     const [bgPercentPos, setBGPercentPos] = useState<number[]>([0, 0]);
 
+    const canvasRef: any = useRef(null);
+
     useEffect(() => {
         if (!width || !height) return;
 
         const pW = state.x / width;
         const pH = state.y / height;
 
-        // const dx = pW - 0.5;
-        // const dy = pH - 0.5;
+        const dx = pW - 0.5;
+        const dy = pH - 0.5;
 
-        // const angle = (Math.atan(dy / dx) * 180) / Math.PI;
-        // setBGAngle(angle + (dx >= 0 ? 180 : 0));
+        const angle = (Math.atan(dy / dx) * 180) / Math.PI;
+        setBGAngle(angle + (dx >= 0 ? 180 : 0));
         setBGPercentPos([pW, pH]);
     }, [state]);
+
+    useEffect(() => {
+        // setFade(true);
+
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        const ctx = canvas?.getContext("2d");
+        if (!ctx) return;
+
+        const widthResolution = 1920;
+
+        canvas.width = widthResolution;
+        canvas.height = (widthResolution / 1920) * 1080;
+
+        const width = canvas.width;
+        const height = canvas.height;
+
+        ctx.lineWidth = 1;
+        const heightDelta = 1;
+        const pipeRatio = 8;
+
+        let heightDiff = 0;
+        let gradientDiff = 0;
+
+        function animate(ctx: CanvasRenderingContext2D) {
+            if (!ctx) return;
+
+            const gradient = ctx.createLinearGradient(
+                0,
+                0 - gradientDiff,
+                0,
+                height * 3 - gradientDiff
+            );
+
+            const color1 = "transparent";
+            const color2 = "#6e56cf";
+
+            gradient.addColorStop(0, color1);
+            gradient.addColorStop(0.1666, color2);
+            gradient.addColorStop(0.3333, color1);
+            gradient.addColorStop(0.5, color2);
+            gradient.addColorStop(0.6666, color1);
+            gradient.addColorStop(0.8333, color2);
+            gradient.addColorStop(1, color1);
+
+            ctx.strokeStyle = gradient;
+
+            ctx.clearRect(0, 0, width, height);
+
+            ctx.beginPath();
+
+            for (let i = -2; i <= 0; i++) {
+                paths.forEach(arr => {
+                    ctx.moveTo(
+                        Math.floor(width * arr[0]),
+                        Math.floor(height * i + heightDiff)
+                    );
+                    ctx.bezierCurveTo(
+                        Math.floor(arr[1] * width),
+                        Math.floor(height * (0.333 + i) + heightDiff),
+                        Math.floor(arr[2] * width),
+                        Math.floor(height * (0.666 + i) + heightDiff),
+                        Math.floor(arr[3] * width),
+                        Math.floor(height * (1 + i) + heightDiff)
+                    );
+                });
+            }
+
+            ctx.stroke();
+            ctx.closePath();
+
+            heightDiff += heightDelta;
+            gradientDiff += heightDelta * pipeRatio;
+
+            if (gradientDiff >= height * 1.5) gradientDiff -= height;
+            if (heightDiff >= height * 1.5) heightDiff -= height;
+
+            requestAnimationFrame(() => animate(ctx));
+        }
+
+        animate(ctx);
+    }, []);
 
     return (
         <>
@@ -48,11 +130,18 @@ export default function HomePage() {
                     background: `radial-gradient(circle at ${bgPercentPos[0] * 100}% ${bgPercentPos[1] * 100}%, var(--violet-3), var(--accent-1))`,
                 }}
             />
-            <div className={styles.waveWrapper} />
             <div className={styles.backgroundNoise} />
             <ScrollDownIndicator
                 stopDisplayingAt={120}
                 style={{ opacity: +fade }}
+            />
+            <canvas
+                className={styles.canvas}
+                ref={canvasRef}
+                style={{
+                    opacity: +fade,
+                    transform: "scale(1.5) rotate(30deg)",
+                }}
             />
             <Container>
                 <Section className={styles.lander} style={{ opacity: +fade }}>
