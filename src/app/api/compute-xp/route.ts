654a5f933bb79580c9aebd653d5167aef69c2558
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2024-07-17 11:45:18 -0700

    Implement xpFormula

--- src/app/api/compute-xp/route.ts
+++ src/app/api/compute-xp/route.ts
@@ -25,5 +25,23 @@ export async function POST(request: NextRequest) {
 }
 
 function xpFormula(mapRating: number, category: Category, accuracy: number, rate: number) {
-  return accuracy * mapRating
+  const thresholds = [70, 80,  85,  90,  93,   95,   97,  98,   99]
+  const bias =       [0.1, 0.5, 0.6, 0.7, 0.77, 0.82, 0.9, 0.95, 1]
+  
+  let bottom = 0
+  
+  thresholds.forEach((t) => {
+      if (t < accuracy) bottom = t
+  })
+  
+  const tRange = [bottom, thresholds[thresholds.indexOf(bottom) + 1]]
+  const bRange = tRange.map((t) => bias[thresholds.indexOf(t)])
+
+  const ratingCoefficient = lerp(accuracy, tRange[0], tRange[1], bRange[0], bRange[1])
+  return ratingCoefficient * mapRating
+}
+
+function lerp(v: number, s1: number, e1: number, s2: number, e2: number) {
+  const p = (v - s1) / (e1 - s1)
+  return (e2 - s2) * p + s2
 }
