60a502dc19fd546fea6b1959e48ba6631225571d
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-07-26 18:38:50 -0700

    Force rating input to use new cookies

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,6 +5,7 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import prisma from "../../prisma/initialize";
 import GenerateHash from "@/lib/generateHash";
+import { User } from "@prisma/client";
 
 export async function Logout() {
     cookies().delete("user_id");
@@ -14,19 +15,19 @@ export async function Logout() {
     redirect("/");
 }
 
-export async function GetUser() {
+export async function GetUser(): Promise<User | undefined> {
     const user_id = cookies().get("user_id")?.value;
     const hash = cookies().get("hash")?.value;
 
-    if (!user_id) return;
+    if (!user_id) return undefined;
 
     const user = await prisma.user.findUnique({
         where: { user_id: parseInt(user_id as string) },
     });
 
-    if (GenerateHash(user?.quaver_id ?? 0) !== (hash as string)) {
+    if (GenerateHash(user?.quaver_id ?? 0) !== (hash as string) || !user) {
         Logout();
-        return null;
+        return undefined;
     }
 
     return user;
