b4fcce038912f77494048740a40ae9b7d1a2e4b5
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-07-28 21:20:54 -0700

    Remove blur for nav(bugged when HWA is off)

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -56,8 +56,8 @@ export default function NavBar({ user, hash }: NavbarProps) {
         <nav
             className={styles.navBar}
             style={{
-                backgroundColor: showBG ? "var(--accent-3)" : "rgba(0,0,0,0)",
-                backdropFilter: showBG ? "blur(10px)" : "none",
+                backgroundColor: showBG ? "var(--accent-a3)" : "rgba(0,0,0,0)",
+                backdropFilter: showBG ? "brightness(0.2) sepia(0.3)" : "none",
                 marginTop: `${path === "/" ? Math.max(35 - windowScrollValue, 0) : 0}px`,
             }}
         >
